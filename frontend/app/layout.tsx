b0bb6d3f303a819fe50ddd809d2ade2c4c517ac9
Harsh31105 <harshnirajsingh@gmail.com>
2025-11-12 02:33:21 -0500

    feat: NavBar Hi-Fis!

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,14 @@
 import "./globals.css";
 import NavBar from "../components/NavBar";
 
-import { Geist, Geist_Mono, Solway, Kantumruy_Pro, Sedgwick_Ave_Display } from "next/font/google";
+import {
+    Geist,
+    Geist_Mono,
+    Solway,
+    Kantumruy_Pro,
+    Sedgwick_Ave_Display,
+    Manrope
+} from "next/font/google";
 import type { ReactNode } from "react";
 
 const geistSans  = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
@@ -9,13 +16,15 @@ const geistMono  = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"
 const solway     = Solway({ variable: "--font-solway", weight: "700", subsets: ["latin"] });
 const kantumruy  = Kantumruy_Pro({ variable: "--font-kantumruy", weight: ["400","500","700"], subsets: ["latin"] });
 const sedgwick   = Sedgwick_Ave_Display({ variable: "--font-sedgwick", weight: "400", subsets: ["latin"] });
+const manrope = Manrope({ variable: "--font-manrope", weight: "600", subsets: ["latin"] })
 
 export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
   return (
     <html lang="en" className={`${kantumruy.variable} ${sedgwick.variable}`}>
-      <body className={`${geistSans.variable} ${geistMono.variable} ${solway.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} ${solway.variable} 
+                        ${manrope.variable}`}>
         <NavBar />
-        <main className="pt-16">{children}</main>
+        <main>{children}</main>
       </body>
     </html>
   );
